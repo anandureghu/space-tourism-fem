17fc44014618527ccaff8bff56d3c74478434ec4
anandureghu <anandukr145@gmail.com>
2023-08-30 17:45:42 +0530

    added destination page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import HomePage from "./pages/HomePage/HomePage";
 import { useSettings } from "./hooks/useSettings";
+import DestinationPage from "./pages/DestinationPage/DestinationPage";
 
 function App() {
   const { activeIndex } = useSettings();
@@ -10,9 +11,9 @@ function App() {
     <>
       <Navbar />
       {activeIndex === 0 && <HomePage />}
-      {activeIndex === 1 && <HomePage />}
+      {activeIndex === 1 && <DestinationPage />}
       {activeIndex === 2 && <HomePage />}
-      {activeIndex === 3 && <HomePage />}
+      {activeIndex === 3 && <DestinationPage />}
     </>
   );
 }
---
+++ src/pages/DestinationPage/DestinationPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./DestinationPage.scss";
+
+const DestinationPage = () => {
+  return <div className="destination">
+    <div className="destination__header">
+      
+    </div>
+  </div>;
+};
+
+export default DestinationPage;
