1e7f3dd160df7ef26dae72befa3bf9c0a20001f5
anandureghu <anandukr145@gmail.com>
2023-08-31 13:50:04 +0530

    implemented destination page

--- src/pages/DestinationPage/DestinationPage.jsx
+++ src/pages/DestinationPage/DestinationPage.jsx
@@ -1,12 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "./DestinationPage.scss";
+import { useSettings } from "../../hooks/useSettings";
+import { H5 } from "../../shared/typography";
+import Tab from "../../components/Tab/Tab";
+import DestinationDetails from "../../components/DescriptionDetails/DestinationDetails";
+
+const destinations = ["Moon", "Mars", "Europa", "Titan"];
 
 const DestinationPage = () => {
-  return <div className="destination">
-    <div className="destination__header">
-      
+  const { activeIndex } = useSettings();
+  const [selectedDestination, setSelectedDestination] = useState(
+    destinations[0]
+  );
+
+  const handleTabChange = (tab) => {
+    setSelectedDestination(tab);
+  };
+
+  return (
+    <div className="destination">
+      <div className="destination__header">
+        <H5>
+          <span className="destination__header-subheading">0{activeIndex}</span>
+          &nbsp; Pick your destination
+        </H5>
+      </div>
+      <div className="destination__content">
+        <div className="destination__content-image">
+          <img
+            src={`/assets/destination/image-${selectedDestination.toLowerCase()}.webp`}
+            alt={selectedDestination}
+          />
+        </div>
+        <div className="destination__content-data">
+          <Tab onChange={handleTabChange} tabs={destinations} />
+          <DestinationDetails destination={selectedDestination} />
+        </div>
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default DestinationPage;
